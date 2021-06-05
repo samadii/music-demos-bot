6a95590492c4e275c6c581005cdaa42e3235b085
samadii <77415087+samadii@users.noreply.github.com>
2021-06-05 15:08:05 +0430

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import requests
 import time
 import mutagen
 from mutagen.mp3 import MP3
-from mutagen.mp4 import MP4
 from telegram.ext import Updater, Bot, MessageHandler, Filters, Handler
 
 TOKEN = os.environ.get('BOT_TOKEN')
