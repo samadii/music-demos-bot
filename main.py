f595e13c5e9f4c185db60f2359475286778dfca6
samadii <77415087+samadii@users.noreply.github.com>
2021-06-05 16:27:15 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import os
+import sys
 import telegram
 import logging
 import requests
 import time
 import mutagen
 from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
 from telegram.ext import Updater, Bot, MessageHandler, Filters, Handler
 
 TOKEN = os.environ.get('BOT_TOKEN')
+
+
+def handle(bot):
+    if update.effective_message.audio:
+        try:
+            fileid = update.effective_message.audio.file_id
+            print(fileid)
+            getfile = bot.get_file(fileid).download()
+            filename = getfile
+            if ".mp3" in filename:
+                audio = MP3(filename)
+                length = audio.info.length * 0.33
+                l2 = (audio.info.length * 0.33) + 60
+            if ".m4a" in filename:
+                audio = MP4(filename)
+                length = audio.info.length * 0.33
+                l2 = (audio.info.length * 0.33) + 60
+            if audio.info.length > l2:
+                os.system("ffmpeg -ss " + str(length) + " -t 60 -y -i \"" + filename + "\" -ac 1 -map 0:a -codec:a libopus -b:a 128k -vbr off -ar 24000 temp/output.ogg")
+            else:
+                os.system("ffmpeg -ss 0 -t 60 -y -i \"" + filename + "\" -ac 1 -map 0:a -codec:a libopus -b:a 128k -vbr off -ar 24000 temp/output.ogg")
+            sendVoice(update.effective_message.chat_id, "temp/output.ogg","")
+        except:
+            pass
